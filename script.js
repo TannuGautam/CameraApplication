c482ac42ba5a16ac17af03947b80cc59d3966002
TannuGautam <tannu8975@gmail.com>
2021-05-27 19:30:52 +0530

    added colored filters for video and added time when recording is on

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 let videoElement = document.querySelector("video");
 
-isRecording = false;
-
+let isRecording = false;
 
 let recordBtn = document.querySelector(".record");
+
+let captureImgBtn = document.querySelector(".capture");
+
+let filterArr = document.querySelectorAll(".filter");
+
+let filterOverlay = document.querySelector(".filter_overlay");
+
+let filterColor= "";
+
+//timer 
+let timings = document.querySelector(".timing");
+
+let counter = 0;
+
+let clearObj;
 
 //user requirement send ie audio n video requirement
 
@@ -74,11 +88,105 @@ recordBtn.addEventListener("click", function()
     {
         mediarecordingObjectForCurrStream.start();
         recordBtn.innerText = "Recording.....";
+
+        //timer started --> startTimer() function called
+        startTimer();
+
     }
     else
     {
+        //stop timer -> stopTimer() function call
+        stopTimer();
         mediarecordingObjectForCurrStream.stop();
         recordBtn.innerText = "Record";
     }
     isRecording = !isRecording; // making it false if true
 })
+
+
+
+captureImgBtn.addEventListener("click", function()
+{
+    //canvas create
+    let canvas = document.createElement("canvas");
+
+    canvas.height = videoElement.videoHeight;
+
+    canvas.width = videoElement.videoWidth;
+
+    let tool = canvas.getContext("2d");
+
+    tool.drawImage(videoElement, 0 , 0);
+
+    //adding filters to video element that will be captured.
+    if(filterColor)
+    {
+        tool.fillStyle = filterColor;
+
+        tool.fillRect(0,0,canvas.width,canvas.height);
+    }
+
+    let url = canvas.toDataURL();
+
+    let a = document.createElement("a");
+
+    a.download = "file.png";
+
+    a.href = url;
+
+    a.click();
+
+    a.remove();
+})
+
+//filter array
+for(let i = 0; i < filterArr.length; i++)
+{
+    filterArr[i].addEventListener("click", function()
+    {
+        filterColor = filterArr[i].style.backgroundColor;
+
+        filterOverlay.style.backgroundColor = filterColor;
+    })
+}
+
+//timer logiccc
+
+function startTimer()
+{
+    timings.style.display = "block";
+
+    function fn()
+    {
+        //hours
+        let hours = Number.parseInt(counter / 3600);
+
+        //seconds
+        let remSeconds = counter % 3600;
+
+        let seconds = remSeconds % 60;
+
+        //minutes
+        let mins = Number.parseInt(remSeconds / 60);
+
+        hours = hours < 10 ? `0${hours}` : hours;
+
+        seconds = seconds < 10 ? `0${seconds}` : seconds;
+
+        mins = mins < 10 ? `0${mins}` : mins;
+
+        timings.innerText = `${hours}:${mins}:${seconds}`;
+
+        counter++;
+
+    }
+
+    clearObj = setInterval(fn, 1000);
+    
+    function stopTimer()
+    {
+        timings.style.display = "none";
+
+        clearInterval(clearObj);
+    }
+}
